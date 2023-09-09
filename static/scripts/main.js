b4af57b9e49180fd50085294f5071cd198e6991e
Thybow <tmilville.pro@gmail.com>
2023-09-09 19:31:13 +0200

    bug - source main.js

---
+++ static/scripts/main.js
@@ -0,0 +1,24 @@
+let currentStep = 1;
+const form = document.getElementById("multistep-form");
+const slides = document.querySelectorAll(".slide");
+
+function nextStep() {
+  if (currentStep < slides.length) {
+    slides[currentStep - 1].style.display = "none";
+    currentStep++;
+    slides[currentStep - 1].style.display = "block";
+  }
+}
+
+function prevStep() {
+  if (currentStep > 1) {
+    slides[currentStep - 1].style.display = "none";
+    currentStep--;
+    slides[currentStep - 1].style.display = "block";
+  }
+}
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  // Traitez les données du formulaire ici
+});

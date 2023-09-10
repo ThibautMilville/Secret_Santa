4a13cee874f0262997c6f1906fe6a9db75f2e761
Thybow <tmilville.pro@gmail.com>
2023-09-10 11:30:14 +0200

    test

--- app.py
+++ app.py
@@ -17,5 +17,5 @@ def index():
     return render_template('index.html', participants=participants)
 
 # Run the app in local
-#if __name__ == '__main__':
-    #app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
